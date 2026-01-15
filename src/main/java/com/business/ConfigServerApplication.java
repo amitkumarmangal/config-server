0daaf86d07a1e74e0e3b64b25bec4fd8b24186f3
Amit Mangal <amit.mangal01@nagarro.com>
2026-01-15 12:55:20 +0530

    Base Commit

--- src/main/java/com/business/ConfigServerApplication.java
+++ src/main/java/com/business/ConfigServerApplication.java
@@ -1,8 +1,12 @@
+package com.business;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.business")
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
